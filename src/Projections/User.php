f449822dad73e24ab59a9cc5a61e9124906e528f
A G <fdwnct@gmail.com>
2025-03-22 20:39:26 -0400

    setup phpstan, lvl 8 passing

--- src/Projections/User.php
+++ src/Projections/User.php
@@ -4,14 +4,25 @@ declare(strict_types=1);
 
 namespace XbNz\LaravelAuditableUsers\Projections;
 
+use DateTimeImmutable;
 use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Ramsey\Uuid\UuidInterface;
 use Spatie\EventSourcing\Projections\Projection;
 use XbNz\LaravelAuditableUsers\Casts\UuidCast;
 use XbNz\LaravelAuditableUsers\Database\Factories\UserFactory;
 
+/**
+ * @property UuidInterface $uuid
+ * @property string $email
+ * @property string|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property DateTimeImmutable $created_at
+ * @property DateTimeImmutable $updated_at
+ */
 final class User extends Projection implements Authenticatable
 {
     use AuthenticatableTrait;
